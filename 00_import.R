68f172c00da7100198a7285494327d912f4148a7
eryamada <yamada.ey@gmail.com>
2017-07-07 07:36:41 -1000

    add initial titanic files

---
+++ 00_import.R
@@ -0,0 +1,20 @@
+
+# initalize ---------------------------------------------------------------
+
+options(scipen=999)
+pacman::p_load(data.table, skimr)
+
+
+# load data ---------------------------------------------------------------
+
+train <- fread('data/input/train.csv')
+test <- fread('data/input/test.csv')
+
+
+# combine train and test datasets -----------------------------------------
+
+train$set <- 'train'
+test$set <- 'test'
+dt <- rbind(train, test, fill = TRUE)
+
+
